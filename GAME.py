cf48624c9281759b31fad2836c212ea54aec5e51
Matheus Pereira <matheus_pereira01@hotmail.com>
2022-07-17 21:46:04 -0300

    New

--- GAME.py
+++ GAME.py
@@ -16,28 +16,28 @@ jogador = jogador - 1
 # Validador Pedra
 if jogador == 0:
     if maquina == 0:
-        print(f"EMPATE!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador-1]}")
+        print(f"EMPATE!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador]}")
     elif maquina == 1:
-        print(f"VOCÊ PERDEU!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador-1]}")
+        print(f"VOCÊ PERDEU!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador]}")
     elif maquina == 2:
-        print(f"!!VOCÊ GANHOU!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador-1]}")
+        print(f"!!VOCÊ GANHOU!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador]}")
 
 # Validador Papel
 if jogador == 1:
     if maquina == 1:
-        print(f"EMPATE!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador-1]}")
+        print(f"EMPATE!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador]}")
     elif maquina == 2:
-        print(f"VOCÊ PERDEU!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador-1]}")
+        print(f"VOCÊ PERDEU!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador]}")
     elif maquina == 0:
-        print(f"!!VOCÊ GANHOU!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador-1]}")
+        print(f"!!VOCÊ GANHOU!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador]}")
 
 # Validador Tesoura
 if jogador == 2:
     if maquina == 2:
-        print(f"EMPATE!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador-1]}")
+        print(f"EMPATE!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador]}")
     elif maquina == 0:
-        print(f"VOCÊ PERDEU!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador-1]}")
+        print(f"VOCÊ PERDEU!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador]}")
     elif maquina == 1:
-        print(f"!!VOCÊ GANHOU!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador-1]}")
+        print(f"!!VOCÊ GANHOU!! \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador]}")
 else:
-    print(f'ainda não sei esse resultado \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador-1]}')
+    print(f'ainda não sei esse resultado \n A maquina fez {opcoes[maquina]} \n Você fez {opcoes[jogador]}')
